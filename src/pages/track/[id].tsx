04e48c59039e483ba739bcb144ce246608cb2682
= <prasad.hewage@Chamindas-MacBook-Pro.local>
2023-02-01 02:18:00 +0200

    feat: add track info and image

--- src/pages/track/[id].tsx
+++ src/pages/track/[id].tsx
@@ -13,30 +13,64 @@ const Track: React.FC<{
   console.log("###trackAnalytics info: ", trackAnalytics);
 
   return (
-    <div>
-      <div className="flex flex-wrap md:flex-1 gap-1 pt-1">
-        <div className=" grow  border border-solid border-gray-200 h-52 w-44">
-          01
-        </div>
-        <div className=" border border-solid border-gray-200 h-52 w-52 grow">
-          02
+    <div className="p-3">
+      <div className="flex flex-wrap md:flex-1 gap-1 ">
+        <div className=" grow border border-solid border-gray-200 h-52 w-1/5 hover:shadow-lg p-4">
+          <div className="flex shrink-0 items-start justify-between ">
+            <div>
+              <img
+                src={track.album.images[1].url}
+                style={{
+                  height: "180px",
+                  width: "178px",
+                }}
+                alt=""
+                className="absolute object-cover"
+                loading="lazy"
+              />
+            </div>
+
+            <div>
+              <div className="flex items-start justify-between">
+                <div className="flex flex-col space-y-2">
+                  <span className="text-lg font-semibold">{track.name}</span>
+                  <span className="text-gray-400">
+                    {track.album.release_date}
+                  </span>
+                </div>
+              </div>
+              <div>
+                <span>Popularity </span>
+                <span className="inline-block px-2 text-sm text-white bg-green-300 rounded">
+                  {track.popularity}%
+                </span>
+              </div>
+              <div>
+                <span>Duration </span>
+                <span className="inline-block px-2 text-sm text-white bg-orange-200 rounded">
+                  {Math.round((track.duration_ms * 100) / (60 * 1000)) / 100}{" "}
+                  mins
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="border border-solid border-gray-200 h-52 basis-1/2 grow">
           03
         </div>
       </div>
       <div className="flex flex-wrap md:flex-1 gap-1 pt-1">
-        <div className="basis-1/2 grow   h-1/4 border border-solid border-gray-200 ">
+        <div className="basis-1/2 grow  h-1/4 border border-solid border-gray-200 shadow-md hover:shadow-lg">
           <PictorialBarChart trackFeatures={trackFeatures} />
         </div>
-        <div className=" h-1/4 grow  border border-solid border-gray-200">
+        <div className=" h-1/4 grow  border border-solid border-gray-200  shadow-md hover:shadow-lg">
           <div className="flex place-items-center justify-center">
             <RadarChart trackFeatures={trackFeatures} />
           </div>
         </div>
       </div>
       <div className="flex flex-wrap md:flex-1 gap-1 pt-1">
-        <div className="grow border border-solid border-gray-200 h-52 w-42">
+        <div className="grow border border-solid border-gray-200 h-52 w-42 shadow-md hover:shadow-lg">
           06
         </div>
       </div>
